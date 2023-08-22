c64a35e31fef295641ec6ae4f52501ddd7374fdf
TuanVan61312 <vantuan01219230619@gmail.com>
2023-08-22 13:47:26 +0700

    22/08/2023

--- src/GreatestCommonFactor.java
+++ src/GreatestCommonFactor.java
@@ -1,2 +1,27 @@
-package PACKAGE_NAME;public class GreatestCommonFactor {
+import java.util.Scanner;
+
+public class GreatestCommonFactor {
+    public static void main(String[] args) {
+        int a = 0;
+        int b = 0;
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter a: ");
+        a = input.nextInt();
+        System.out.println("Enter b: ");
+        b = input.nextInt();
+        a = Math.abs(a);
+        b = Math.abs(b);
+
+        if(a == 0 || b == 0)
+            System.out.println("No greatest common factor ");
+        else {
+            while (a != b) {
+                if (a > b)
+                    a = a - b;
+                else
+                    b = b - a;
+            }
+            System.out.println("Greatest common factor: " + a);
+        }
+    }
 }
--- src/Prime.java
+++ src/Prime.java
@@ -1,2 +1,24 @@
-package PACKAGE_NAME;public class Prime {
+import java.util.Scanner;
+
+public class Prime {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int number = input.nextInt();
+        if(number >= 2){
+            boolean check = true;
+            for(int i = 2; i <= Math.sqrt(number); i++){
+                if(number % i == 0){
+                    check = false;
+                    break;
+                }
+            }
+            if(check)
+                System.out.println(number + " is a prime");
+            else
+                System.out.println(number + " is not a prime");
+        } else {
+            System.out.println(number + " is not a prime");
+        }
+    }
 }
